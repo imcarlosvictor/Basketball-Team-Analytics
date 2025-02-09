e1bcf837d211f302d85c25f2e7dfed181539232e
Lucas <imcvlucas@gmail.com>
2025-02-09 01:19:43 -0500

    Completed side bar components and streamlit themes

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,54 @@ class Dashboard:
 
         with st.sidebar:
             options = [
-                'Game 1 | BWB vs. Venoms',
-                "Game 2 | BWB vs. ABG",
-                "Game 3 | BWB vs. GoEasy",
+                "Game 1  | BWB vs. Venoms",
+                "Game 2  | BWB vs. ABG",
+                "Game 3  | BWB vs. GoEasy",
+                "Game 4  | BWB vs. --",
+                "Game 5  | BWB vs. --",
+                "Game 6  | BWB vs. --",
+                "Game 7  | BWB vs. --",
+                "Game 8  | BWB vs. --",
+                "Game 9  | BWB vs. --",
+                "Game 10 | BWB vs. --",
+                "Game 11 | BWB vs. --",
+                "Game 12 | BWB vs. --",
+                "Game 13 | BWB vs. --",
+                "Game 14 | BWB vs. --",
             ]
             selected_option = st.sidebar.selectbox("Game", options)
 
         with st.sidebar:
             options = [
-                'David',
-                'Noel',
+                "Team Overall",
+                "David",
+                "Noel",
+                "Josh",
+                "Jake",
+                "John",
+                "James",
+                "Marcus",
+                "Ricky",
+                "Harry",
+                "Jeff",
             ]
             selected_option = st.sidebar.selectbox("Player", options)
 
+        leading_stats_container = st.container(border=True)
+        col_1, col_2, col_3, col_4 = st.columns(4)
 
+        with col_1:
+            card_1 = st.container(border=True)
+            st.subheader(":fire: Leading Scorer :fire:")
+
+        with col_2:
+            card_2 = st.container(border=True)
+            st.subheader("Most Field Goals Made")
+
+        with col_3:
+            card_3 = st.container(border=True)
+            st.subheader("Most 3PT Field Goals made")
+
+        with col_4:
+            card_4 = st.container(border=True)
+            st.subheader("Free Throw Percentage")
