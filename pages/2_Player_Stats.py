ac92d216d6291ace2bc0d70fc388f02e3362c4bd
Lucas <imcvlucas@gmail.com>
2025-03-04 17:33:48 -0500

    completed layout for career/season statistics

--- pages/2_Player_Stats.py
+++ pages/2_Player_Stats.py
@@ -69,11 +69,11 @@ class PlayerStatisticsDashboard:
             # First, build a default layout for every element you want to include in your dashboard
             layout = [
                 # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-                dashboard.Item('radar_graph', 0, 0, 4, 4, isResizable=True, isDraggable=True, moved=False),
-                dashboard.Item('heatmap_block', 4, 0, 8, 4, isResizable=True, isDraggable=True, moved=False),
-                dashboard.Item('career_stats_block', 0, 5, 4, 2.6, isResizable=True, isDraggable=True, moved=False),
-                dashboard.Item('season_stats_block', 0, 9, 4, 2.6, isResizable=True, isDraggable=True, moved=False),
-                dashboard.Item('game_log', 4, 0, 8, 5.2, isResizable=True, isDraggable=True, moved=False),
+                dashboard.Item('radar_graph', 0, 0, 4, 4, isResizable=False, isDraggable=False, moved=False),
+                dashboard.Item('heatmap_block', 4, 0, 8, 4, isResizable=False, isDraggable=False, moved=False),
+                dashboard.Item('career_stats_block', 0, 5, 4, 2.4, isResizable=False, isDraggable=False, moved=False),
+                dashboard.Item('season_stats_block', 0, 9, 4, 2.4, isResizable=False, isDraggable=False, moved=False),
+                dashboard.Item('game_log', 4, 0, 8, 5.0, isResizable=False, isDraggable=False, moved=False),
             ]
 
             mui.Box(
@@ -179,8 +179,10 @@ class PlayerStatisticsDashboard:
         html.div(
             block_title,
             css={
-                'padding': '.5em .5em .5em 2em',
-                'backgroundColor': '#dfdfdf',
+                'padding': '.5em .5em .5em .7em',
+                'margin-left': '1em',
+                'margin-right': '1.8em',
+                'border-bottom': 'solid .7px #232323',
                 'color': '#232323',
                 'display': 'grid',
                 'justify-content': 'start',
@@ -194,8 +196,6 @@ class PlayerStatisticsDashboard:
 
         with elements('statistics_overview_dashboard'):
             BOX_CSS = {
-                'height': 5,
-                'width': 10,
                 'p': 0,
                 'display': 'flex',
                 'flex-direction': 'column',
@@ -206,9 +206,28 @@ class PlayerStatisticsDashboard:
                 }
             }
 
+            FFI_BOX_CSS = {
+                'p': 0,
+                'display': 'flex',
+                'radius': 3,
+                'flex-direction': 'column',
+                'justify-content': 'center',
+                'align-items': 'center',
+                'background': '#e3e3e3',
+            }
+
             TITLE_CSS = {
                 'color': '#232323',
                 'font-size': 15,
+            }
+
+            FFI_TITLE_CSS = {
+                # 'overflow-wrap': 'break-word',
+                # 'inline-size': 110,
+                'word-break': 'break-all',
+                'color': '#232323',
+                'font-size': 11,
+                'padding-bottom': '.5em',
             }
 
             VALUE_CSS = {
@@ -217,48 +236,40 @@ class PlayerStatisticsDashboard:
                 'font-weight': 'medium',
             }
 
-            FFI_BOX_CSS = {
-                'p': 0,
-                'display': 'flex',
-                'radius': 3,
-                'flex-direction': 'column',
-                'justify-content': 'center',
-                'align-items': 'center',
-                'background': '#6c45fe',
+            GAIN_DELTA_CSS = {
+                'color': '#16a68e',
+                'font-size': 14,
+                'font-weight': 'bold',
             }
 
-            FFI_TITLE_CSS = {
-                'color': '#ececec',
-                'font-size': 15,
-            }
-
-            FFI_VALUE_CSS = {
-                'color': '#ececec',
-                'font-size': 22,
-                'font-weight': 'medium',
+            LOSS_DELTA_CSS = {
+                'color': '#ff1400',
+                'font-size': 14,
+                'font-weight': 'bold',
             }
 
             # First, build a default layout for every element you want to include in your dashboard
             layout = [
                 # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+                # ROW 1
                 dashboard.Item('PTS', 0, 0, .7, .5, isResizable=False, isDraggable=False, isBounded=True),
                 dashboard.Item('AST', .7, 0, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('STL', 1.4, 0, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('BLK', 2.1, 0, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('EFGP', 2.8, 0, 1, .5, isResizable=False, isDraggable=False, moved=False), # FOUR FACTOR INDEX
-
+                # ROW 2
                 dashboard.Item('ORB', 0, .5, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('DRB', .7, .5, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('FOUL', 1.4, .5, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('TUR', 2.1, .5, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('TURR', 2.8, .5, 1, .5, isResizable=False, isDraggable=False, moved=False), # FOUR FACTOR INDEX
-
+                # ROW 3
                 dashboard.Item('FG', 0, 1, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('FG%', .7, 1, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('FT', 1.4, 1, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('FT%', 2.1, 1, .7, .5, isResizable=False, isDraggable=False, moved=False),
                 dashboard.Item('ORB%', 2.8, 1, 1, .5, isResizable=False, isDraggable=False, moved=False), # FOUR FACTOR INDEX
-
+                # ROW 4
                 dashboard.Item('2P', 0, 1.5, .7, .5, isResizable=False, isDraggable=True, moved=False),
                 dashboard.Item('2P%', .7, 1.5, .7, .5, isResizable=False, isDraggable=True, moved=False),
                 dashboard.Item('3P', 1.4, 1.5, .7, .5, isResizable=False, isDraggable=True, moved=False),
@@ -266,15 +277,17 @@ class PlayerStatisticsDashboard:
                 dashboard.Item('FTR', 2.8, 1.5, 1, .5, isResizable=False, isDraggable=False, moved=False), # FOUR FACTOR INDEX
             ]
 
-            with dashboard.Grid(layout):
+            with dashboard.Grid(layout, autoSize=True, margin=[0,0], containerPadding=[0,0]):
                 # ----------------- ROW 1 -----------------
                 with mui.Box(key='PTS', sx=BOX_CSS):
                     mui.Box('PTS', sx=TITLE_CSS)
                     mui.Box(data['pts'], sx=VALUE_CSS)
+                    mui.Box('+2.2', sx=GAIN_DELTA_CSS)
 
                 with mui.Box(key='AST', sx=BOX_CSS):
                     mui.Box('AST', sx=TITLE_CSS)
                     mui.Box(data['ast'], sx=VALUE_CSS)
+                    mui.Box('+0.6', sx=LOSS_DELTA_CSS)
 
                 with mui.Box(key='STL', sx=BOX_CSS):
                     mui.Box('STL', sx=TITLE_CSS)
@@ -285,14 +298,15 @@ class PlayerStatisticsDashboard:
                     mui.Box(data['blk'], sx=VALUE_CSS)
 
                 with mui.Box(key='EFGP', sx=FFI_BOX_CSS): # FOUR FACTOR INDEX
-                    mui.Box('EFGP', sx=FFI_TITLE_CSS)
-                    mui.Box(data['2p'], sx=FFI_VALUE_CSS)
+                    mui.Box('Effective FG%', sx=FFI_TITLE_CSS)
+                    mui.Box(data['2p'], sx=VALUE_CSS)
 
             # ----------------- ROW 2 -----------------
-            with dashboard.Grid(layout):
+            with dashboard.Grid(layout, margin=[0,0]):
                 with mui.Box(key='ORB', sx=BOX_CSS):
                     mui.Box('ORB', sx=TITLE_CSS)
                     mui.Box(data['orb'], sx=VALUE_CSS)
+                    mui.Box('+2.2', sx=GAIN_DELTA_CSS)
 
                 with mui.Box(key='DRB', sx=BOX_CSS):
                     mui.Box('DRB', sx=TITLE_CSS)
@@ -307,11 +321,11 @@ class PlayerStatisticsDashboard:
                     mui.Box(data['tur'], sx=VALUE_CSS)
 
                 with mui.Box(key='TURR', sx=FFI_BOX_CSS): # FOUR FACTOR INDEX
-                    mui.Box('TURR', sx=FFI_TITLE_CSS)
-                    mui.Box(data['2p%'], sx=FFI_VALUE_CSS)
+                    mui.Box('Turnover Ratio', sx=FFI_TITLE_CSS)
+                    mui.Box(data['2p%'], sx=VALUE_CSS)
 
             # ----------------- ROW 3 -----------------
-            with dashboard.Grid(layout):
+            with dashboard.Grid(layout, margin=[0,0]):
                 with mui.Box(key='FG', sx=BOX_CSS):
                     mui.Box('FG', sx=TITLE_CSS)
                     mui.Box(data['fg'], sx=VALUE_CSS)
@@ -329,11 +343,11 @@ class PlayerStatisticsDashboard:
                     mui.Box(data['ft%'], sx=VALUE_CSS)
 
                 with mui.Box(key='ORB%', sx=FFI_BOX_CSS): # FOUR FACTOR INDEX
-                    mui.Box('ORB%', sx=FFI_TITLE_CSS)
-                    mui.Box(data['3p'], sx=FFI_VALUE_CSS)
+                    mui.Box('Offensive Rebound %', sx=FFI_TITLE_CSS)
+                    mui.Box(data['3p'], sx=VALUE_CSS)
 
             # ----------------- ROW 4 -----------------
-            with dashboard.Grid(layout):
+            with dashboard.Grid(layout, margin=[0,0]):
                 with mui.Box(key='2P', sx=BOX_CSS):
                     mui.Box('2P', sx=TITLE_CSS)
                     mui.Box(data['2p'], sx=VALUE_CSS)
@@ -351,8 +365,25 @@ class PlayerStatisticsDashboard:
                     mui.Box(data['3p%'], sx=VALUE_CSS)
 
                 with mui.Box(key='FTR', sx=FFI_BOX_CSS): # FOUR FACTOR INDEX
-                    mui.Box('FTR', sx=FFI_TITLE_CSS)
-                    mui.Box(data['3p%'], sx=FFI_VALUE_CSS)
+                    mui.Box('Free Throw Ratio', sx=FFI_TITLE_CSS)
+                    mui.Box(data['3p%'], sx=VALUE_CSS)
+
+        html.div(
+            '4-Factors Index: 28.5',
+            css={
+                'margin-bottom': '1em',
+                'padding': '.5em 2em .5em .5em',
+                # 'backgroundColor': '#dfdfdf',
+                'color': '#232323',
+                'display': 'grid',
+                'justify-content': 'end',
+                'align-items': 'start',
+                'text-transform': 'uppercase',
+                'font-family': 'sans-serif',
+                'font-stlye': 'italic',
+                'font-size': '14px',
+            }
+        )
 
 
     def radial_bar(self):
@@ -370,8 +401,6 @@ class PlayerStatisticsDashboard:
 
             nivo.RadialBar(
                 data=DATA,
-                # width=275,
-                # height=250,
                 padding=0.5,
                 valueFormat='>-.2f',
                 startAngle=-124,
@@ -380,13 +409,8 @@ class PlayerStatisticsDashboard:
                 cornerRadius=2,
                 margin={'top': 20, 'right': 20, 'bottom': 20, 'left': 20},
                 colors = ['#07da63', '#b4b9c7'],
-                # colors = [ '#07da63', '#f5c512', '#e5e5e5'],
-                # colors = [ '#1eb281', '#f3e05e', '#dc143c', '#e5e5e5'],
-                # colorBy='index',
-                # colors={'scheme': 'red_yellow_green'},
                 borderColor={'from': 'color', 'modifiers': {'darker': 1}},
                 enableTracks=True,
-                # tracksColor='#e3e3e3',
                 enableRadialGrid=False,
                 enableCircularGrid=False,
                 radialAxisStart=None,
@@ -441,7 +465,7 @@ class PlayerStatisticsDashboard:
                 # colors = [ '#8ab8fe', '#359982', '#005ffd'],
                 # colors = [ '#e7b7f4', '#9488d8', '#dc143c'],
                 # colors = [ '#ffaf31', '#ff7600', '#ff398e'],
-                colors = [ '#ffaf31', '#ff4655', '#6c45fe'],
+                colors = [ '#ffaf31', '#ff4655', '#008080'],
                 colorBy='index',
                 curve='catmullRomClosed',
                 fillOpacity=0.35,
